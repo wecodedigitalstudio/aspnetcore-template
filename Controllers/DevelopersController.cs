c309f484ff2c3f628f6f2a0386c9af1663923050
Giacomo Corallini <corallinigiacomo89@gmail.com>
2018-10-16 16:59:39 +0200

    bootstrap applicazione + varie dipendenze

---
+++ Controllers/DevelopersController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using aspnetcore_template.Models;
+
+namespace aspnetcore_template.Controllers
+{
+    public class DevelopersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
